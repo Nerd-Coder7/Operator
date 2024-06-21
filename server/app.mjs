029c8b59fc26ef1297cdbcf9a2b2b6957546d250
AronWeb <dev@aron123>
2024-06-21 13:49:59 +0530

    --fg

--- server/app.mjs
+++ server/app.mjs
@@ -15,8 +15,8 @@ app.use(morgan("dev"));
 app.use(cookieParser());
 const corsOptions = {
   // origin: process.env.ORIGIN,
-  // origin: 'https://operator-steel.vercel.app', 
-  origin: 'http://localhost:3000', 
+  origin: 'https://operator-steel.vercel.app', 
+  // origin: 'http://localhost:3000', 
   credentials: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
   allowedHeaders: ['Content-Type', 'Authorization'] 
