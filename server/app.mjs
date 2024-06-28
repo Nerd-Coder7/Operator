83a82192564630d9f270d5fd2474584b095231d9
Nerd-Coder7 <126475820+Nerd-Coder7@users.noreply.github.com>
2024-06-28 13:46:20 +0530

    Update app.mjs

--- server/app.mjs
+++ server/app.mjs
@@ -15,7 +15,7 @@ app.use(morgan("dev"));
 app.use(cookieParser());
 const corsOptions = {
   // origin: process.env.ORIGIN,
-  origin: 'https://operator-steel.vercel.app', 
+  origin: 'https://clever-noether.5-250-185-202.plesk.page', 
   // origin: 'http://localhost:3000', 
   credentials: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
